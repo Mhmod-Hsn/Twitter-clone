d20df9132cc00ccf9b18f1e68eeff79c956a5f4f
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-05-13 01:21:35 +0200

    like dislike

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,27 +1,34 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const mongoose = require("mongoose")
+const { ObjectId } = mongoose.Schema.Types
+const Schema = mongoose.Schema
 
 const postSchema = new Schema({
-    user: {
-        type: Schema.Types.Object,
-        required: true,
+  user: {
+    type: Schema.Types.Object,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: false,
+  },
+  postImage: {
+    type: String,
+    required: false,
+  },
+  tags: {
+    type: Array,
+    required: false,
+  },
+  likes: [
+    {
+      type: ObjectId,
+      ref: "User",
     },
-    text: {
-        type: String,
-        required: false,
-    },
-    postImage:{
-        type: String,
-        required: false
-    },
-    tags:{
-        type: Array,
-        required: false
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now()
-    },
-});
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+})
 
-module.exports = mongoose.model('Post', postSchema);
+module.exports = mongoose.model("Post", postSchema)
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,173 +1,156 @@
-const router = require('express').Router();
-const Post = require('../models/Post');
+const router = require("express").Router()
+const Post = require("../models/Post")
 
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
+const jwt = require("jsonwebtoken")
+const passport = require("passport")
 
-const validateRegisterInput = require('../validation/register');
-const validateLoginInput = require('../validation/login');
+const validateRegisterInput = require("../validation/register")
+const validateLoginInput = require("../validation/login")
 
-const multer = require('multer');
+const multer = require("multer")
 
 const storage = multer.diskStorage({
-    destination: function (req,file,cb) {
-        cb(null, 'uploads/')
-    },
-    filename: function (req, file, cb) {
-        cb(null, new Date().getTime() + '--' + file.originalname)
-    }
-});
+  destination: function (req, file, cb) {
+    cb(null, "uploads/")
+  },
+  filename: function (req, file, cb) {
+    cb(null, new Date().getTime() + "--" + file.originalname)
+  },
+})
 
 const fileFilter = (req, file, cb) => {
-    // reject
-    if (['image/jpeg','image/png'].indexOf(file.mimetype) > -1){
-        cb(null,true)
-    } else {
-        cb(null,false)
-    }
+  // reject
+  if (["image/jpeg", "image/png"].indexOf(file.mimetype) > -1) {
+    cb(null, true)
+  } else {
+    cb(null, false)
+  }
 }
 
 const limits = {
-    fileSize: 1024*1024*1
+  fileSize: 1024 * 1024 * 1,
 }
-
 
 // multer options
 const upload = multer({
-    storage,
-    limits,
-    fileFilter
-});
+  storage,
+  limits,
+  fileFilter,
+})
 
+router
+  .route("/add")
+  .post(
+    upload.single("postImage"),
+    passport.authenticate("jwt", { session: false }),
+    (req, res) => {
+      const newPost = new Post({
+        user: {
+          id: req.user.id,
+          username: req.user.username,
+        },
+        text: req.body.text || null,
+        tags: req.body.tags.split(",") || null,
+        postImage: req.file ? req.file.path : null,
+        createdAt: Date.now(),
+      })
 
+      newPost
+        .save()
+        .then((post) => res.json(post))
+        .catch((err) => console.log(err))
+    },
+  )
 
+router
+  .route("/like/")
+  .put(passport.authenticate("jwt", { session: false }), (req, res) => {
+    Post.findByIdAndUpdate(
+      req.body.postId,
+      {
+        $push: {
+          likes: req.user._id,
+        },
+      },
+      {
+        new: true,
+      },
+    )
+      .then((success) => res.json({ message: "Update successful!" }))
+      .catch((err) => console.log(err))
+  })
 
-router.route('/add')
-    .post(
-        upload.single('postImage'),
-        passport.authenticate(
-        'jwt',
-        {session: false}),
-        (req, res) => {
+router
+  .route("/unlike/")
+  .put(passport.authenticate("jwt", { session: false }), (req, res) => {
+    Post.findByIdAndUpdate(
+      req.body.postId,
+      {
+        $pull: {
+          likes: req.user._id,
+        },
+      },
+      {
+        new: true,
+      },
+    )
+      .then((success) => res.json({ message: "Update successful!" }))
+      .catch((err) => console.log(err))
+  })
 
-            const newPost = new Post({
-                user: {
-                    id: req.user.id,
-                    username: req.user.username,
-                },
-                text: req.body.text || null,
-                tags: req.body.tags.split(',') || null,
-                postImage: req.file? req.file.path : null,
-                createdAt: Date.now()
-            });
+router
+  .route("/:postId")
+  .put(passport.authenticate("jwt", { session: false }), (req, res) => {
+    const text = req.body.text.trim()
 
+    const edits = {
+      $set: {
+        text: text,
+      },
+    }
 
-            newPost.save()
-                .then(post => res.json(post))
-                .catch(err => console.log(err))
-        });
+    Post.updateOne({ _id: req.params.postId }, edits)
+      .then((success) => res.json({ message: "Update successful!" }))
+      .catch((err) => console.log(err))
+  })
 
+router
+  .route("/:postId")
+  .delete(passport.authenticate("jwt", { session: false }), (req, res) => {
+    Post.deleteOne({ _id: req.params.postId }, function (err) {
+      if (err) {
+        res.status(404).json({ message: "Wrong ID" })
+      } else {
+        res.json({ message: "Deleted successful!" })
+      }
+    })
+  })
 
-router.route('/:postId')
-    .put(passport.authenticate(
-        'jwt',
-        {session: false}),
-        (req, res) => {
-            const text = req.body.text.trim();
+router
+  .route("/following")
+  .get(passport.authenticate("jwt", { session: false }), (req, res) => {
+    Post.find({
+      "user.id": {
+        $in: req.user.following,
+      },
+    })
+      .sort({ createdAt: -1 })
+      .then((posts) => res.json(posts))
+      .catch((err) => console.log(err))
+  })
 
+router.route("/user/:userId").get((req, res) => {
+  Post.find({ "user.id": req.params.userId })
+    .sort({ createdAt: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => console.log(err))
+})
 
-            const edits = {
-                $set: {
-                    text: text
-                }
-            };
+router.route("/").get((req, res) => {
+  Post.find()
+    .sort({ createdAt: -1 })
+    .then((posts) => res.json(posts))
+    .catch((err) => console.log(err))
+})
 
-
-            Post.updateOne(
-                {_id: req.params.postId},
-                edits
-            )
-                .then(success => res.json({message: "Update successful!"}))
-                .catch(err => console.log(err))
-        });
-
-
-router.route('/:postId')
-    .delete(passport.authenticate(
-        'jwt',
-        {session: false}),
-        (req, res) => {
-
-
-            Post.deleteOne(
-                {_id: req.params.postId}, function (err) {
-                    if (err){
-                        res.status(404).json({message: "Wrong ID"})
-                    }
-                    else{
-                        res.json({message: "Deleted successful!"})
-                    }
-                })
-        });
-
-
-router.route('/following')
-    .get(passport.authenticate(
-        'jwt',
-        {session: false}),
-        (req, res) => {
-            Post.find({
-                'user.id': {
-                    $in: req.user.following
-                }
-            })
-                .sort({createdAt: -1})
-                .then(posts => res.json(posts))
-                .catch(err => console.log(err))
-        });
-
-
-router.route('/user/:userId')
-    .get((req, res) => {
-        Post.find({'user.id': req.params.userId})
-            .sort({createdAt: -1})
-            .then(posts => res.json(posts))
-            .catch(err => console.log(err))
-    });
-
-
-router.route('/')
-    .get((req, res) => {
-        Post.find()
-            .sort({createdAt: -1})
-            .then(posts => res.json(posts))
-            .catch(err => console.log(err))
-    });
-
-
-
-
-router.route('/xml')
-    .get((req, res) => {
-        Post.find()
-            .then(posts => res.send(
-                toXml(posts)
-            ))
-            .catch(err => console.log(err))
-    });
-const toXml = (data) => {
-    return '<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope/" soap:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><soap:Body><m:GetPostsResponse>'+
-        data.reduce((result, el) => {
-            return result + `
-                <m:PostDetails>
-                    <m:PostId>${el._id}</m:PostId>
-                    <m:PostTitle> ${el.text}</m:PostTitle>
-                    <m:PostUser> ${el.user.username} </m:PostUser>
-                </m:PostDetails>
-`
-        }, '')
-        +'</m:GetPostsResponse> </soap:Body></soap:Envelope>'
-}
-
-
-module.exports = router;
+module.exports = router
